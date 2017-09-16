9dc317c9302cac620300fa3bb229cd536ca96266
DUY <duybeo1509@gmail.com>
2017-09-16 14:20:53 +0700

    update UIHome

--- NhaTro/NhaTro/AppDelegate.swift
+++ NhaTro/NhaTro/AppDelegate.swift
@@ -20,6 +20,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate{
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        UITabBar.appearance().tintColor = .black
+
 //        FirebaseApp.configure()
         GIDSignIn.sharedInstance().clientID = FirebaseApp.app()?.options.clientID
 
