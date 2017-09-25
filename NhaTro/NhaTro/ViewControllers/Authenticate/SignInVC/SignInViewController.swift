aec5a75ebd9e35b3b3af2dc6fd8a72e0309f9c95
HOANGHUNG <hung.nguyenhoang@apps-cyclone.com>
2017-09-25 21:00:57 +0700

    Add comment in SignInVC

--- NhaTro/NhaTro/ViewControllers/Authenticate/SignInVC/SignInViewController.swift
+++ NhaTro/NhaTro/ViewControllers/Authenticate/SignInVC/SignInViewController.swift
@@ -16,6 +16,8 @@ class SignInViewController: UIViewController {
     @IBOutlet weak var txtEmail: UITextField!
     @IBOutlet weak var txtPassword: UITextField!
 
+    
+    //MARK:- Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
@@ -39,11 +41,12 @@ class SignInViewController: UIViewController {
     }
     
     
-    
+    //MARK:- Action buttons
     @IBAction func Back(_ sender: UIButton) {
         dismiss(animated: true, completion: nil)
 
     }
+    
     @IBAction func SignIn(_ sender: UIButton) {
         guard let email = self.txtEmail.text, let pass = self.txtPassword.text else {
             return
@@ -60,6 +63,7 @@ class SignInViewController: UIViewController {
 //        self.present(tabbar, animated: true, completion: nil)
 
     }
+    
     @IBAction func RegisterEmail(_ sender: UIButton) {
         let registerVC = Storyboard.main.instantiateViewController(withIdentifier: "RegisterViewController") as! RegisterViewController
         self.pushTo(registerVC)
@@ -71,7 +75,11 @@ class SignInViewController: UIViewController {
 //MARK:- Support API
 extension SignInViewController {
     
-    
+    fileprivate func callAPILogin(_ params:[String:AnyObject]) {
+        
+        
+        
+    }
     
     
 }
