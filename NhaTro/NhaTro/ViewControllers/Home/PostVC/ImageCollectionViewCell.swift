2e9bd38cdfb52171fc5b6458ed532df8dcaea24f
DUY <duybeo1509@gmail.com>
2017-10-17 16:50:31 +0700

    update UI New

---
+++ NhaTro/NhaTro/ViewControllers/Home/PostVC/ImageCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ImageCollectionViewCell.swift
+//  NhaTro
+//
+//  Created by DUY on 10/17/17.
+//  Copyright © 2017 HOANG HUNG. All rights reserved.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imgHinhCell: UIImageView!
+}
