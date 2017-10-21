0d58ceb4160c7d5832d11e9b11880817cd4fdccf
HOANGHUNG <hung.nguyenhoang@apps-cyclone.com>
2017-10-21 12:55:14 +0700

    Remove SWRevealViewController

--- NhaTro/NhaTro/Libraries/SWRevealViewController/NhaTro-Bridging-Header.h
+++ NhaTro/NhaTro/Libraries/SWRevealViewController/NhaTro-Bridging-Header.h
@@ -2,8 +2,8 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import "SWRevealViewController.h"
 #import <CommonCrypto/CommonCrypto.h>
 #import <AsyncDisplayKit/AsyncDisplayKit.h>
+
 
 
