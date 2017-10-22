6286778c78bd6e10a1736556b6bea107d1dbbada
HOANGHUNG <hung.nguyenhoang@apps-cyclone.com>
2017-10-22 09:38:48 +0700

    Fix warning in Xcode 9

--- NhaTro/NhaTro/Configs/Extensions/UIViewExtension.swift
+++ NhaTro/NhaTro/Configs/Extensions/UIViewExtension.swift
@@ -161,7 +161,7 @@ class PaddingTextField: UITextField {
 
 extension NSLayoutConstraint {
     func constraintWithMultiplier(_ multiplier: CGFloat) -> NSLayoutConstraint {
-        return NSLayoutConstraint(item: self.firstItem, attribute: self.firstAttribute, relatedBy: self.relation, toItem: self.secondItem, attribute: self.secondAttribute, multiplier: multiplier, constant: self.constant)
+        return NSLayoutConstraint(item: self.firstItem!, attribute: self.firstAttribute, relatedBy: self.relation, toItem: self.secondItem, attribute: self.secondAttribute, multiplier: multiplier, constant: self.constant)
     }
 }
 
